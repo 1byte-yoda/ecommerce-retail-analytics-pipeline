4ece546f483c94a9fc9998126d93572c0914b1c3
Mark Caneda <us66@klavenessdigital.com>
2023-10-10 22:39:01 +0800

    Dim Order Status DataFrame Creation

---
+++ src/spark/olist_dwh/silver_transformer/dim_order_status.py
@@ -0,0 +1,14 @@
+from pyspark.sql import DataFrame, Column
+from pyspark.sql.window import Window
+import pyspark.sql.functions as F
+
+
+def _get_order_status_id() -> Column:
+    id_window_spec = Window.partitionBy(F.col("order_status")).orderBy(F.col("order_status"))
+    return F.row_number().over(id_window_spec)
+
+
+def create_dim_order_status_df(orders_df: DataFrame) -> DataFrame:
+    order_status_id = _get_order_status_id()
+    dim_order_status_df = orders_df.select("order_status").distinct().withColumn("order_status_id", order_status_id)
+    return dim_order_status_df

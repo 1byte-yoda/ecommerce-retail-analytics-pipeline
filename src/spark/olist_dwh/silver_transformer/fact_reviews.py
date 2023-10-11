025284dd0a8c3b4a088e054b6e6761066182918d
Mark Caneda <us66@klavenessdigital.com>
2023-10-11 23:23:40 +0800

    Fact Reviews DataFrame Creation

---
+++ src/spark/olist_dwh/silver_transformer/fact_reviews.py
@@ -0,0 +1,24 @@
+from pyspark.sql import DataFrame, SparkSession
+import pyspark.sql.functions as F
+
+
+def create_fact_reviews_df(reviews_df: DataFrame, orders_df: DataFrame, dim_date_df: DataFrame) -> DataFrame:
+    orders_df = orders_df.withColumnRenamed("order_status", "o_order_status")
+    reviews_df = reviews_df.withColumnRenamed("order_id", "v_order_id")
+
+    fact_reviews_df = reviews_df.join(orders_df, on=F.col("order_id") == F.col("v_order_id"), how="inner") \
+        .join(dim_date_df.alias("d1"), on=dim_date_df["unix_timestamp"] == F.unix_timestamp("review_creation_date"), how="inner") \
+        .join(dim_date_df.alias("d2"), on=dim_date_df["unix_timestamp"] == F.unix_timestamp("review_answer_timestamp"), how="inner") \
+        .selectExpr(
+            "review_id",
+            "order_id",
+            "customer_id",
+            "order_status_id",
+            "d1.date_id AS review_creation_date_id",
+            "d2.date_id AS review_answer_timestamp_id",
+            "review_score",
+            "review_comment_title",
+            "review_comment_message"
+        )
+
+    return fact_reviews_df

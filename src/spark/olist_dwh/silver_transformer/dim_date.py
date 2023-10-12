1f3b100990e15237b20471d5101a9d2f620fc7b0
Mark Caneda <us66@klavenessdigital.com>
2023-10-12 18:40:20 +0800

    Correction with date dimension generation logic

--- src/spark/olist_dwh/silver_transformer/dim_date.py
+++ src/spark/olist_dwh/silver_transformer/dim_date.py
@@ -1,44 +1,48 @@
 from datetime import datetime, timedelta
 from typing import List, Tuple
 
-from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql import DataFrame, SparkSession, Window
 import pyspark.sql.functions as F
-from pyspark.sql.types import StructType, StructField, TimestampType, IntegerType, StringType, LongType
+from pyspark.sql.types import StructType, StructField, TimestampType, IntegerType, StringType, DoubleType
 
 
-def generate_date_list(min_dates: List[datetime], max_dates: List[datetime]) -> List[Tuple]:
-    start_date, end_date = max(max_dates), min(min_dates)
-    n_days = (end_date - start_date).days
-    return [(x + 1, (start_date + timedelta(days=x)).timestamp(), start_date + timedelta(days=x)) for x in range(0, n_days)]
+def generate_series(spark: SparkSession, start: str, stop: str, interval: int):
+    start, stop = spark.createDataFrame([(start, stop)], ("start", "stop")).select(
+        [F.col(c).cast("timestamp").cast("long") for c in ("start", "stop")]
+    ).first()
+
+    return spark.range(start, stop + 1, interval).select(
+        F.row_number().over(Window.orderBy("id")).alias("date_id"),
+        F.unix_timestamp(F.col("id").cast("timestamp")).alias("unix_timestamp"),
+        F.col("id").cast("timestamp").alias("timestamp")
+    )
 
 
 def get_date_generated_schema() -> StructType:
     return StructType(
         [
             StructField("date_id", IntegerType(), nullable=False),
-            StructField("unix_timestamp", LongType(), nullable=False),
+            StructField("unix_timestamp", DoubleType(), nullable=False),
             StructField("timestamp", TimestampType(), nullable=False)
         ]
     )
 
 
 def _create_initial_dim_date_df(spark: SparkSession, min_dates: List[datetime], max_dates: List[datetime]):
-    date_generated = generate_date_list(min_dates=min_dates, max_dates=max_dates)
-    schema = get_date_generated_schema()
-    return spark.createDataFrame(data=date_generated, schema=schema)
+    start_date, end_date = min(min_dates), max(max_dates)
+    return generate_series(spark=spark, start=start_date.isoformat(), stop=end_date.isoformat(), interval=1)
 
 
 def create_dim_date_df(spark: SparkSession, min_dates: List[datetime], max_dates: List[datetime]) -> DataFrame:
     dim_date_df = _create_initial_dim_date_df(spark=spark, min_dates=min_dates, max_dates=max_dates)
     udf_get_week_day_type = F.udf(lambda x: "weekend" if x in ("Sat", "Sun") else "weekday", StringType())
-
     dim_date_df = dim_date_df.withColumn("year", F.date_format(F.col("timestamp"), "yyyy")) \
         .withColumn("quarter", F.date_format(F.col("timestamp"), "Q")) \
         .withColumn("quarter_name", F.date_format(F.col("timestamp"), "qq")) \
         .withColumn("month", F.date_format(F.col("timestamp"), "M")) \
         .withColumn("month_name", F.date_format(F.col("timestamp"), "MMM")) \
-        .withColumn("year_month", F.date_format(F.col("timestamp"), "yyyy-MM")) \
-        .withColumn("year_week", F.concat(F.date_format(F.col("timestamp"), "yyyy"), "-", F.weekofyear(F.col("timestamp")))) \
+        .withColumn("year_month", F.date_format(F.col("timestamp"), "yyyyMM")) \
+        .withColumn("year_week", F.concat(F.date_format(F.col("timestamp"), "yyyy"), F.weekofyear(F.col("timestamp")))) \
         .withColumn("week_day_name", F.date_format(F.col("timestamp"), "EEE")) \
         .withColumn("week_day_type", udf_get_week_day_type(F.date_format(F.col("timestamp"), "EEE"))) \
         .withColumn("timezone", F.date_format(F.col("timestamp"), "zzz")) \
@@ -51,7 +55,7 @@ def create_dim_date_df(spark: SparkSession, min_dates: List[datetime], max_dates
             ).when(
                 F.date_format(F.col("timestamp"), "HH:mm").between("12:00", "18:00"), "Afternoon"
             ).when(
-                F.date_format(F.col("order_purchase_timestamp"), "HH:mm").between("18:01", "24:00"), "Night"
+                F.date_format(F.col("timestamp"), "HH:mm").between("18:01", "24:00"), "Night"
             )
         )
     return dim_date_df

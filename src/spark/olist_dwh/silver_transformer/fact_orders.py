3264489add514a33624ed4dd0e4c7cdc73babefb
Mark Caneda <us66@klavenessdigital.com>
2023-10-12 20:05:24 +0800

    Removed unused joins and fixed name typo error

--- src/spark/olist_dwh/silver_transformer/fact_orders.py
+++ src/spark/olist_dwh/silver_transformer/fact_orders.py
@@ -9,15 +9,12 @@ def process_dim_date_df(spark: SparkSession, dim_date_df: DataFrame, orders_df: 
     return spark.sql(
         """
             SELECT  o.order_id AS d_order_id
-                    # ,d1.date_id AS shipment_limit_date_id
                     ,d2.date_id AS order_purchase_timestamp_id
                     ,d3.date_id AS order_approved_at_id
                     ,d4.date_id AS order_delivered_carrier_date_id
                     ,d5.date_id AS order_delivered_customer_date_id
                     ,d6.date_id AS order_estimated_delivery_date_id
             FROM orders AS o
-            JOIN order_items AS oit ON oit.order_id = o.order_id
-            # LEFT JOIN dim_date AS d1 ON d1.unix_timestamp = UNIX_TIMESTAMP(oit.shipment_limit_date)
             LEFT JOIN dim_date AS d2 ON d2.unix_timestamp = UNIX_TIMESTAMP(o.order_purchase_timestamp)
             LEFT JOIN dim_date AS d3 ON d3.unix_timestamp = UNIX_TIMESTAMP(o.order_approved_at)
             LEFT JOIN dim_date AS d4 ON d4.unix_timestamp = UNIX_TIMESTAMP(o.order_delivered_carrier_date)
@@ -36,8 +33,8 @@ def create_fact_orders_df(
 
     fact_orders_df = orders_df.join(order_items_df, on=F.col("order_id") == F.col("order_item_order_id"), how="inner") \
         .join(dim_order_status_df, on=F.col("o_order_status") == F.col("order_status"), how="inner") \
-        .join(dim_date_df.alias("shipment_limit"), on=dim_date_df["unix_timestamp"] == F.unix_timestamp("shipment_limit_date")) \
-        .join(processed_dim_date_df, on=F.col("d_order_id") == F.unix_timestamp("order_id"), how="inner") \
+        .join(dim_date_df.alias("shipping_limit"), on=dim_date_df["unix_timestamp"] == F.unix_timestamp("shipping_limit_date")) \
+        .join(processed_dim_date_df, on=F.col("d_order_id") == F.col("order_id"), how="inner") \
         .selectExpr(
             "order_id",
             "order_item_id",
@@ -45,7 +42,7 @@ def create_fact_orders_df(
             "seller_id",
             "customer_id",
             "order_status_id",
-            "shipment_limit.date_id AS shipment_limit_date_id",
+            "shipping_limit.date_id AS shipping_limit_date_id",
             "order_purchase_timestamp_id",
             "order_approved_at_id",
             "order_delivered_carrier_date_id",

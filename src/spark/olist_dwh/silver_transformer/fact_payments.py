6900133b0e8ea58b8b3e1c7241a79c8415ade11c
Mark Caneda <us66@klavenessdigital.com>
2023-10-12 21:50:20 +0800

    Renamed order_status DF

--- src/spark/olist_dwh/silver_transformer/fact_payments.py
+++ src/spark/olist_dwh/silver_transformer/fact_payments.py
@@ -1,6 +1,7 @@
 import sys
 
 sys.path.append("/sources/spark_app/")  # noqa
+sys.path.append("../../../src/spark/olist_dwh/silver_transformer")  # noqa
 
 from pyspark.sql import DataFrame, SparkSession
 import pyspark.sql.functions as F
@@ -9,14 +10,14 @@ from fact_orders import process_dim_date_df
 
 
 def create_fact_payments_df(
-    spark: SparkSession, payments_df: DataFrame, orders_df: DataFrame, order_status_df: DataFrame, dim_date_df: DataFrame
+    spark: SparkSession, payments_df: DataFrame, orders_df: DataFrame, dim_order_status_df: DataFrame, dim_date_df: DataFrame
 ) -> DataFrame:
     payments_df = payments_df.withColumnRenamed("order_id", "p_order_id")
     orders_df = orders_df.withColumnRenamed("order_status", "o_order_status")
     processed_dim_date_df = process_dim_date_df(spark=spark, dim_date_df=dim_date_df, orders_df=orders_df)
 
     fact_payments_df = payments_df.join(orders_df, on=F.col("order_id") == F.col("p_order_id"), how="inner") \
-        .join(order_status_df, on=F.col("order_status") == F.col("o_order_status"), how="inner") \
+        .join(dim_order_status_df, on=F.col("order_status") == F.col("o_order_status"), how="inner") \
         .join(processed_dim_date_df, on=F.col("d_order_id") == F.col("order_id"), how="inner") \
         .select(
             "order_id",

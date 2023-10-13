32976961959244b32bda31c1240badfacd849361
Mark Caneda <us66@klavenessdigital.com>
2023-10-13 17:58:33 +0800

    Used a merged dates dataframe for generation of dim_date dataframe

--- src/spark/olist_dwh/silver_transformer/dim_date.py
+++ src/spark/olist_dwh/silver_transformer/dim_date.py
@@ -1,48 +1,19 @@
-from datetime import datetime
-from typing import List
-
 from pyspark.sql import DataFrame, SparkSession
 import pyspark.sql.functions as F
-from pyspark.sql.types import StructType, StructField, TimestampType, IntegerType, StringType, DoubleType
+from pyspark.sql.types import IntegerType, StringType
 
 
-def generate_series(spark: SparkSession, start: str, stop: str, interval: int):
-    start, stop = spark.createDataFrame([(start, stop)], ("start", "stop")).select(
-        [F.col(c).cast("timestamp").cast("long") for c in ("start", "stop")]
-    ).first()
-
-    return spark.range(start, stop + 1, interval).select(
-        F.monotonically_increasing_id().alias("date_id"),
-        F.unix_timestamp(F.col("id").cast("timestamp")).alias("unix_timestamp"),
-        F.col("id").cast("timestamp").alias("timestamp")
-    )
-
-
-def get_date_generated_schema() -> StructType:
-    return StructType(
-        [
-            StructField("date_id", IntegerType(), nullable=False),
-            StructField("unix_timestamp", DoubleType(), nullable=False),
-            StructField("timestamp", TimestampType(), nullable=False)
-        ]
-    )
-
-
-def _create_initial_dim_date_df(spark: SparkSession, min_dates: List[datetime], max_dates: List[datetime]):
-    start_date, end_date = min(min_dates), max(max_dates)
-    return generate_series(spark=spark, start=start_date.isoformat(), stop=end_date.isoformat(), interval=1)
-
-
-def create_dim_date_df(spark: SparkSession, min_dates: List[datetime], max_dates: List[datetime]) -> DataFrame:
-    dim_date_df = _create_initial_dim_date_df(spark=spark, min_dates=min_dates, max_dates=max_dates)
+def create_dim_date_df(date_df: DataFrame) -> DataFrame:
     udf_get_week_day_type = F.udf(lambda x: "weekend" if x in ("Sat", "Sun") else "weekday", StringType())
-    dim_date_df = dim_date_df.withColumn("year", F.date_format(F.col("timestamp"), "yyyy")) \
-        .withColumn("quarter", F.date_format(F.col("timestamp"), "Q")) \
+    dim_date_df = date_df.withColumn("date_id", F.monotonically_increasing_id()) \
+        .withColumn("year", F.date_format(F.col("timestamp"), "yyyy").cast(IntegerType())) \
+        .withColumn("unix_timestamp", F.unix_timestamp(F.col("timestamp").cast("timestamp"))) \
+        .withColumn("quarter", F.date_format(F.col("timestamp"), "Q").cast(IntegerType())) \
         .withColumn("quarter_name", F.date_format(F.col("timestamp"), "qqq")) \
-        .withColumn("month", F.date_format(F.col("timestamp"), "M")) \
+        .withColumn("month", F.date_format(F.col("timestamp"), "M").cast(IntegerType())) \
         .withColumn("month_name", F.date_format(F.col("timestamp"), "MMM")) \
-        .withColumn("year_month", F.date_format(F.col("timestamp"), "yyyyMM")) \
-        .withColumn("year_week", F.concat(F.date_format(F.col("timestamp"), "yyyy"), F.weekofyear(F.col("timestamp")))) \
+        .withColumn("year_month", F.date_format(F.col("timestamp"), "yyyyMM").cast(IntegerType())) \
+        .withColumn("year_week", F.concat(F.date_format(F.col("timestamp"), "yyyy"), F.weekofyear(F.col("timestamp"))).cast(IntegerType())) \
         .withColumn("week_day_name", F.date_format(F.col("timestamp"), "EEE")) \
         .withColumn("week_day_type", udf_get_week_day_type(F.date_format(F.col("timestamp"), "EEE"))) \
         .withColumn("timezone", F.date_format(F.col("timestamp"), "zzz")) \

ef833def1c9033da7f7e1941804a75cf97ae4ad2
Mark Caneda <us66@klavenessdigital.com>
2023-10-13 22:14:51 +0800

    Gold Layer Table Creation for Olist Reporting

--- src/dags/ecommerce_product_pipeline.py
+++ src/dags/ecommerce_product_pipeline.py
@@ -28,18 +28,18 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id="2lux_ecommerce_data_pipeline",
+    dag_id=spark_app_name,
     description="This is the data pipeline for the Olist Ecommerce Data Platform.",
     default_args=default_args,
     schedule_interval=timedelta(days=1)
 )
 
-bronze_ecommerce_product = DummyOperator(task_id="bronze_ecommerce_job", dag=dag)
+bronze_layer_stage = DummyOperator(task_id="bronze_layer_stage", dag=dag)
 
-silver_ecommerce_transformer_job = SparkSubmitOperator(
-    task_id="silver_ecommerce_transformer_job",
+silver_layer_transformer_job = SparkSubmitOperator(
+    task_id="silver_layer_transformer_job",
     application="/sources/spark_app/olist_dwh/silver_transformer/silver_transformer.py",
-    name=spark_app_name,
+    name=f"{spark_app_name}_silver",
     conn_id="spark",
     verbose=False,
     num_executors=2,
@@ -50,6 +50,18 @@ silver_ecommerce_transformer_job = SparkSubmitOperator(
     dag=dag
 )
 
-end = DummyOperator(task_id="end", dag=dag)
+gold_layer_transformer_job = SparkSubmitOperator(
+    task_id="gold_layer_transformer_job",
+    application="/sources/spark_app/olist_dwh/gold_transformer/gold_transformer.py",
+    name=f"{spark_app_name}_gold",
+    conn_id="spark",
+    verbose=False,
+    num_executors=2,
+    executor_cores=1,
+    driver_memory="500m",
+    executor_memory="1700m",
+    jars=get_jars(),
+    dag=dag
+)
 
-bronze_ecommerce_product >> silver_ecommerce_transformer_job >> end
+bronze_layer_stage >> silver_layer_transformer_job >> gold_layer_transformer_job
---
+++ src/spark/olist_dwh/gold_transformer/gold_transformer.py
@@ -0,0 +1,93 @@
+import sys
+
+sys.path.append("/sources/spark_app/")  # noqa
+
+from delta import configure_spark_with_delta_pip
+from pyspark.sql import SparkSession, DataFrame
+
+from helper import overwrite_to_table
+
+
+def create_order_performance_df(spark: SparkSession) -> DataFrame:
+    return spark.sql("""
+        SELECT d_date1.year
+            ,d_date1.year_month_start
+            ,d_date1.month
+            ,d_date1.year_week
+            ,d_date1.week_start
+            ,d_date1.time_of_day
+            ,dos.order_status
+            ,dc.customer_state
+            ,dc.customer_city
+            ,COUNT(DISTINCT fact_orders.order_id) AS total_orders
+            ,ROUND(SUM(price), 2) AS total_amount
+            ,ROUND(AVG(freight_value), 2) AS avg_freight
+            ,ROUND(IF(AVG(DATEDIFF(d_date1.timestamp, d_date3.timestamp)) < 0, 0, AVG(DATEDIFF(d_date1.timestamp, d_date3.timestamp))), 2) AS avg_days_delivery_delay
+            ,ROUND(AVG(DATEDIFF(d_date1.timestamp, d_date2.timestamp)), 2) AS avg_days_to_deliver
+        FROM silver.fact_orders
+        JOIN silver.dim_order_status dos ON dos.order_status_id = fact_orders.order_status_id
+        LEFT JOIN silver.dim_date d_date1 ON d_date1.date_id = fact_orders.order_delivered_customer_date_id
+        LEFT JOIN silver.dim_date d_date2 ON d_date2.date_id = fact_orders.order_purchase_timestamp_id
+        LEFT JOIN silver.dim_date d_date3 ON d_date3.date_id = fact_orders.order_estimated_delivery_date_id
+        JOIN silver.dim_customers dc ON dc.customer_id = fact_orders.customer_id
+        GROUP BY dos.order_status
+            ,dc.customer_state
+            ,dc.customer_city
+            ,d_date1.year
+            ,d_date1.year_month_start
+            ,d_date1.month
+            ,d_date1.year_week
+            ,d_date1.week_start
+            ,d_date1.time_of_day
+    """)
+
+
+def create_order_payment_channel_report_df(spark: SparkSession) -> DataFrame:
+    return spark.sql("""
+        SELECT d_date1.year
+            ,d_date1.year_month_start
+            ,d_date1.month
+            ,dos.order_status
+            ,dc.customer_state
+            ,dc.customer_city
+            ,COUNT(fact_payments.order_id) AS total_transactions
+            ,ROUND(SUM(fact_payments.payment_value), 2) AS total_payment
+        FROM silver.fact_payments
+        JOIN silver.dim_order_status dos ON dos.order_status_id = fact_payments.order_status_id
+        LEFT JOIN silver.dim_date d_date1 ON d_date1.date_id = fact_payments.order_delivered_customer_date_id
+        JOIN silver.dim_customers dc ON dc.customer_id = fact_payments.customer_id
+        GROUP BY dos.order_status
+            ,dc.customer_state
+            ,dc.customer_city
+            ,d_date1.year
+            ,d_date1.year_month_start
+            ,d_date1.month
+    """)
+
+
+def main(spark: SparkSession):
+    order_performance_df = create_order_performance_df(spark=spark)
+    overwrite_to_table(df=order_performance_df, schema_name="gold", table_name="order_performance_report")
+
+    order_payment_channel_report_df = create_order_payment_channel_report_df(spark=spark)
+    overwrite_to_table(df=order_payment_channel_report_df, schema_name="gold", table_name="order_payment_channel_report")
+
+
+if __name__ == '__main__':
+    SPARK_URI = "spark://spark:7077"
+    HIVE_URI = "thrift://hive-metastore:9083"
+    MINIO_URI = "http://minio:9000"
+
+    builder = SparkSession.builder.appName("olist_silver_transformer").master(SPARK_URI) \
+        .config("spark.hadoop.hive.metastore.uris", HIVE_URI) \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.hadoop.fs.s3a.access.key", "datalake") \
+        .config("spark.hadoop.fs.s3a.secret.key", "datalake") \
+        .config("spark.hadoop.fs.s3a.endpoint", MINIO_URI) \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    spark_session = configure_spark_with_delta_pip(builder).enableHiveSupport().getOrCreate()
+    main(spark=spark_session)
